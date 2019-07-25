48c985c33a79c15bf531b5235b796d83df7446db
recardozo <re.cardozooo@hotmail.com>
2019-07-25 20:30:33 -0300

    Initializing project

---
+++ src/main/java/com/mongo/payhere/PayhereApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.payhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongo/payhere/PayhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongo.payhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
